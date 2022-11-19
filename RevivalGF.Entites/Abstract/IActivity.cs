7b1e15b45619b47c465cf32872d5983aa4596fa9
Mehmet İkbal Dündar <Mehmetikbaldundar@outlook.com>
2022-11-19 21:14:03 +0300

    Recipies - Activities - Suggestions - Settings - DONE !!

--- RevivalGF.Entites/Abstract/IActivity.cs
+++ RevivalGF.Entites/Abstract/IActivity.cs
@@ -15,7 +15,8 @@ namespace RevivalGF.Entites.Abstract
         Activities Activities { get; set; }
         decimal ActivityFaktor { get; set; }       
         decimal Calorie { get; set; }
-        List<User> Users { get; set; }
+        int UserID { get; set; }
+        User User { get; set; }
 
     }
 }
--- RevivalGF.UI/Forms/Recipes.Designer.cs
+++ RevivalGF.UI/Forms/Recipes.Designer.cs
@@ -33,8 +33,8 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.cbRecipes = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.lbRecipes = new System.Windows.Forms.ListBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.rtxtRecipies = new System.Windows.Forms.RichTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pbNext)).BeginInit();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -42,21 +42,21 @@
             // 
             // pbNext
             // 
-            resources.ApplyResources(this.pbNext, "pbNext");
             this.pbNext.BackColor = System.Drawing.Color.Transparent;
             this.pbNext.Image = global::RevivalGF.UI.Properties.Resources.nextbutton;
+            resources.ApplyResources(this.pbNext, "pbNext");
             this.pbNext.Name = "pbNext";
             this.pbNext.TabStop = false;
             this.pbNext.DoubleClick += new System.EventHandler(this.pbNext_DoubleClick);
             // 
             // groupBox1
             // 
-            resources.ApplyResources(this.groupBox1, "groupBox1");
             this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.rtxtRecipies);
             this.groupBox1.Controls.Add(this.cbRecipes);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.lbRecipes);
             this.groupBox1.Controls.Add(this.pictureBox1);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.TabStop = false;
             // 
@@ -65,25 +65,25 @@
             resources.ApplyResources(this.cbRecipes, "cbRecipes");
             this.cbRecipes.FormattingEnabled = true;
             this.cbRecipes.Name = "cbRecipes";
+            this.cbRecipes.SelectedIndexChanged += new System.EventHandler(this.cbRecipes_SelectedIndexChanged);
             // 
             // label1
             // 
             resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
             // 
-            // lbRecipes
-            // 
-            resources.ApplyResources(this.lbRecipes, "lbRecipes");
-            this.lbRecipes.FormattingEnabled = true;
-            this.lbRecipes.Name = "lbRecipes";
-            // 
             // pictureBox1
             // 
-            resources.ApplyResources(this.pictureBox1, "pictureBox1");
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Image = global::RevivalGF.UI.Properties.Resources.frameworkforrecipes;
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.TabStop = false;
+            // 
+            // rtxtRecipies
+            // 
+            resources.ApplyResources(this.rtxtRecipies, "rtxtRecipies");
+            this.rtxtRecipies.Name = "rtxtRecipies";
             // 
             // Recipes
             // 
@@ -93,6 +93,7 @@
             this.Controls.Add(this.pbNext);
             this.Controls.Add(this.groupBox1);
             this.Name = "Recipes";
+            this.Load += new System.EventHandler(this.Recipes_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pbNext)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
@@ -107,7 +108,7 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.ComboBox cbRecipes;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ListBox lbRecipes;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox rtxtRecipies;
     }
 }
